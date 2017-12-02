71e8c2b1f27e4b7beb4046b03d2f34fa4f70dddd
Jorge Manzo <manzoj@oregonstate.edu>
2017-12-01 22:57:20 -0800

    Created a test array
    made an array of test values to be inserted into the array.

--- test.c
+++ test.c
@@ -1,9 +1,19 @@
 
 #include "dynamicArray.h"
 
+struct testNums{
+  int* numbers;
+}
 
 int main(){
   struct dynamicArray* myArray;
+  struct testNums test;
+  test->numbers = (int) malloc(sizeof(int) * 10);
+
+  for(int i = 0; i < 10; i++){
+    test->numbers[i] = i;
+  }
+  
   myArray = dynamicCreate();
 
   dynamicFree(myArray);
