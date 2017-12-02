f77452b63733e53ca564c00f106a1119c68d72fd
Jorge Manzo <manzoj@oregonstate.edu>
2017-12-02 12:39:41 -0800

    Added more tests
    Testing removing from the array.

--- test.c
+++ test.c
@@ -26,13 +26,30 @@ int main(){
   }
 
   myArray = dynamicCreate();
+  //Inserting numbers 1 to 9 into the array
   for(int i = 0; i < 10; i++){
     temp = &(test.numbers[i]);
     dynamicInsert(myArray, i, temp);
+    printf("Inserting At myArray[%d] : %d\n", i, *(int*)dynamicGet(myArray,i));
   }
 
+  //Removing values from index 0 to 4
+  for(int i = 0; i < 5; i++){
+    printf("Removing At myArray[%d] : %d\n", i, *(int*)dynamicGet(myArray,i));
+    dynamicRemove(myArray, i);
+  }
+
+  //Inserting different values in index 0 to 4
+  for(int i = 0; i < 5; i++){
+    temp = &(test.numbers[5 - i]);
+    dynamicInsert(myArray, i, temp);
+    printf("Inserting At myArray[%d] : %d\n", i, *(int*)dynamicGet(myArray,i));
+  }
+
+  //Removing the contents of the array
   for(int i = 0; i < 10; i++){
-    printf("At myArray[%d] : %d\n", i, *(int*)dynamicGet(myArray,i));
+    printf("Removing At myArray[%d] : %d\n", i, *(int*)dynamicGet(myArray,i));
+    dynamicRemove(myArray, i);
   }
 
   dynamicFree(myArray);
