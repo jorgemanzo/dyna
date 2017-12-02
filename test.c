7173cf3b493d048f8eb2aeffb66164f3dbf19768
Jorge Manzo <manzoj@oregonstate.edu>
2017-12-01 22:58:32 -0800

    Wrong operator
    Should be using dot operator instead of arrow to access struct feilds

--- test.c
+++ test.c
@@ -3,17 +3,17 @@
 
 struct testNums{
   int* numbers;
-}
+};
 
 int main(){
   struct dynamicArray* myArray;
   struct testNums test;
-  test->numbers = (int) malloc(sizeof(int) * 10);
+  test.numbers = (int) malloc(sizeof(int) * 10);
 
   for(int i = 0; i < 10; i++){
-    test->numbers[i] = i;
+    test.numbers[i] = i;
   }
-  
+
   myArray = dynamicCreate();
 
   dynamicFree(myArray);
